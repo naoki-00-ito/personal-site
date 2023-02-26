dbbce7abb13c0b0bfec160008ac79c3ab512feff
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-02-26 12:30:47 +0900

    Add: tag component作成

---
+++ components/Tag.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-chip color="pink" text-color="white" label class="mt-2 ml-2">
+    <v-icon class="mr-1">mdi-label</v-icon>
+    {{ tagName }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: "Tag",
+  props: ["tagName"],
+};
+</script>
