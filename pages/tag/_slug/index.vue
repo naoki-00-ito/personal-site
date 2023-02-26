fcfe21f6d85a954fd726c15cb5a28c43804f26a1
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-02-26 14:46:54 +0900

    Update: 記事一覧 component化

--- pages/tag/_slug/index.vue
+++ pages/tag/_slug/index.vue
@@ -1,37 +1,17 @@
 <template>
   <v-row>
     <v-col v-for="article in blog" :key="article.slug" cols="12">
-      <v-card :to="`/blog/${article.slug}`" hover>
-        <v-card-title>{{ article.title }}</v-card-title>
-        <v-card-subtitle>{{ article.createdAt | date }}</v-card-subtitle>
-        <v-divider class="mx-3 my-n1"></v-divider>
-        <v-card-text class="pb-0">
-          <div class="mt-n2 ml-n2">
-            <span v-for="(category, index) in article.category" :key="index" :data-category="category">
-              <Category :categoryName="category" />
-            </span>
-          </div>
-        </v-card-text>
-        <v-card-text>
-          <div class="mt-n2 ml-n2">
-            <span v-for="(tag, index) in article.tags" :key="index" :data-tag="tag">
-              <Tag :tagName="tag" />
-            </span>
-          </div>
-        </v-card-text>
-      </v-card>
+      <ArticleCard :article="article" />
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Category from "@/components/Category"
-import Tag from "@/components/Tag"
+import ArticleCard from "@/components/ArticleCard"
 
 export default {
   components: {
-    Category,
-    Tag,
+    ArticleCard,
   },
   async asyncData({ store, $content, params }) {
     const blog = await $content('blog').where({ tags: { $contains: params.slug }}).sortBy('createdAt', 'desc').fetch();
