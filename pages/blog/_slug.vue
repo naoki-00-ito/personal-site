ca3dda74a80b5e01f269ccb3e1fd87ec4a758765
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-02-26 09:00:20 +0900

    Add: 記事詳細出力作成

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <time :datetime="article.createdAt">
+      {{ article.createdAt | date }}
+    </time>
+    <NuxtContent :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    return {
+      article
+    }
+  }
+}
+</script>
