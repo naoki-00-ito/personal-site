078e56b016bf1a6c1f55cbb0a2f906f4c6da5c2b
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-02-26 12:36:01 +0900

    Add: 記事詳細 tag出力追加

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,11 +4,17 @@
     <time :datetime="article.createdAt">
       {{ article.createdAt | date }}
     </time>
-    <div class="mt-n2 ml-n2">
+    <div class="ml-n2">
       <span v-for="(category, index) in article.category" :key="index" :data-category="category">
         <Category :categoryName="category" />
       </span>
     </div>
+    <div class="ml-n2">
+      <span v-for="(tag, index) in article.tags" :key="index" :data-tag="tag">
+        <Tag :tagName="tag" />
+      </span>
+    </div>
+
     <NuxtContent :document="article" />
   </article>
 </template>
