9d0e475c1e4887f846819f907751fd4100e70272
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-03-02 18:42:54 +0900

    [Add] もっと見るボタン設置

--- pages/category/_slug/index.vue
+++ pages/category/_slug/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-row>
-    <v-col v-for="article in articles" :key="article.slug" cols="12">
-      <ArticleCard :article="article" />
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col v-for="article in articles" :key="article.slug" cols="12">
+        <ArticleCard :article="article" />
+      </v-col>
+    </v-row>
+
+    <div class="text-center mt-10">
+      <v-btn color="primary" :to="'/category/' + $route.params.slug + '/page/1'"
+        >もっと見る</v-btn
+      >
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-row>
-    <v-col v-for="article in articles" :key="article.slug" cols="12">
-      <ArticleCard :article="article" />
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col v-for="article in articles" :key="article.slug" cols="12">
+        <ArticleCard :article="article" />
+      </v-col>
+    </v-row>
+
+    <div class="text-center mt-10">
+      <v-btn color="primary" to="/articles/page/1">もっと見る</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
