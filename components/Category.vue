b26f7befa54d5a2375d891670dcd55a8f05b2682
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-03-14 22:27:10 +0900

    [Update] 記事カテゴリとタグ 名称表示に修正

--- components/Category.vue
+++ components/Category.vue
@@ -1,13 +1,26 @@
 <template>
   <v-chip color="primary" label class="mt-2 ml-2">
     <v-icon class="mr-1">mdi-folder</v-icon>
-    {{ categoryName }}
+    {{ taxonomyname }}
   </v-chip>
 </template>
 
 <script>
+import taxonomys from "@/plugins/taxonomy.js";
+
 export default {
   name: "Category",
   props: ["categoryName"],
+  data() {
+    return {
+      taxonomyname: "",
+    };
+  },
+  async created() {
+    const taxonomy = taxonomys.category.find(
+      (c) => c.slug === this.categoryName
+    );
+    this.taxonomyname = taxonomy ? taxonomy.name : "";
+  },
 };
 </script>
--- components/Tag.vue
+++ components/Tag.vue
@@ -1,13 +1,24 @@
 <template>
   <v-chip color="pink" text-color="white" label class="mt-2 ml-2">
     <v-icon class="mr-1">mdi-label</v-icon>
-    {{ tagName }}
+    {{ taxonomyname }}
   </v-chip>
 </template>
 
 <script>
+import taxonomys from "@/plugins/taxonomy.js";
+
 export default {
   name: "Tag",
   props: ["tagName"],
+  data() {
+    return {
+      taxonomyname: "",
+    };
+  },
+  async created() {
+    const taxonomy = taxonomys.tags.find((c) => c.slug === this.tagName);
+    this.taxonomyname = taxonomy ? taxonomy.name : "";
+  },
 };
 </script>
